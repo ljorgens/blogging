36bd5c54b955323dfc7420cc6982929012920e07
ljorgens <leejorgensen22@gmail.com>
2015-03-20 10:55:58 -0700

    clickable blog post to see body

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -15,4 +15,8 @@ class PostsController < ApplicationController
 			render :new
 		end
 	end
+	
+	def show
+		@post = Post.find(params[:id])
+	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
  resources :posts
  get '/' => "posts#index"
  get 'posts/new' => "posts#new"
+ get 'post' => "posts#show"
 end
